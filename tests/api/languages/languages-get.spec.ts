aa60cee2fdc0925cb2d2ac856afc4105b940a43f
Katarzyna <kankowska.katarzyna@gmail.com>
2024-06-01 23:59:43 +0200

    test(languages-get): add test

--- tests/api/languages/languages-get.spec.ts
+++ tests/api/languages/languages-get.spec.ts
@@ -1,4 +1,6 @@
 import { APIResponse, expect, test } from '@playwright/test';
+import { HttpStatusCode } from '@_src_api/enums/api-status-code.enum';
+
 
 test.describe('GET languages tests', async () => {
 
@@ -7,5 +9,16 @@ test.describe('GET languages tests', async () => {
     const response: APIResponse = await request.get('api/comments');
     // Then
     expect(response.status()).toBe(200);
+  });
+});
+
+test.describe('GET/languages endpoint tests', async () => {
+    const languages: string = '/api/languages';
+
+  test('Returns 200 OK for all comments', async ({ request }) => {
+    // When
+    const response: APIResponse = await request.get(languages);
+    // Then
+    expect(response.status()).toBe(HttpStatusCode.Ok);
   });
 });
