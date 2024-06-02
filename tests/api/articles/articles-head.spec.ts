49b166df0ec2be16c3a5e1422c3ce62eeacf079d
Katarzyna <kankowska.katarzyna@gmail.com>
2024-06-02 21:44:13 +0200

    update articles-head

--- tests/api/articles/articles-head.spec.ts
+++ tests/api/articles/articles-head.spec.ts
@@ -1,3 +1,11 @@
+import { Endpoints } from '@_src_api/enums/api-endpoints.enum';
 import { test } from '@playwright/test';
 
-test.describe('HEAD/articles endpoint tests', async () => {});
+test.describe('HEAD/articles endpoint tests', async () => {
+  test.only('Returns 200 OK for all articles', async ({ request }) => {
+    // When
+    const response: APIResponse = await request.get(Endpoints.ARTICLES);
+    // Then
+    expect(response.status()).toBe(200);
+  });
+});
